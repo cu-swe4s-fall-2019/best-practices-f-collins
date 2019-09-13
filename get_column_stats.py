edb3960d8df5acc57b7b908a06e2adef82351676
Fletcher Collins <fletcher@colli.nz>
2019-09-12 23:51:54 -0600

    modify get_column_stats to conform to best practices

--- get_column_stats.py
+++ get_column_stats.py
@@ -1,19 +1,57 @@
-import sys, math
+import sys
+import math
+import argparse
 
-file_name = sys.argv[1]
-col_num = int(sys.argv[2])
+def main():
+    parser = argparse.ArgumentParser(
+            description='Program to get stats of a column of data.',
+            prog='calculate')
 
-f = open(file_name, 'r')
+    parser.add_argument('--file_name',
+                        type=str,
+                        help='The name of the file to get stats from.',
+                        required=True)
 
-V = []
+    parser.add_argument('--col_num',
+                        type=int,
+                        help='The number of column to be analyzed.',
+                        required=True)
 
-for l in f:
-    A = [int(x) for x in l.split()]
-    V.append(A[col_num])
+    args = parser.parse_args()
 
-mean = sum(V)/len(V)
+    file_name = args.file_name
+    col_num = args.col_num
 
-stdev = math.sqrt(sum([(mean-x)**2 for x in V]) / len(V))
+    try:
+        f = open(file_name, 'r')
+    except FileNotFoundError:
+        print('File does not exist or read access is not given.')
+        quit(1)
 
-print('mean:', mean)
-print('stdev:', stdev)
+    V = []
+
+    try:
+        for l in f:
+            A = [int(x) for x in l.split()]
+            V.append(A[col_num])
+    except IndexError:
+        print('Column does not exist in data file.')
+        print('Ensure that every line has ' + str(args.col_num) + ' values.')
+        print('Reminder that columns are zero-indexed.')
+        quit(1)
+    except ValueError:
+        # could be improved by giving the user the offending character and the
+        # line it was found on.
+        print('Non number found in data.')
+        quit(1)
+
+    mean = sum(V)/len(V)
+
+    stdev = math.sqrt(sum([(mean-x)**2 for x in V]) / len(V))
+
+    print('mean:', mean)
+    print('stdev:', stdev)
+
+
+if __name__ == '__main__':
+    main()
